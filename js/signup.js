90f824573e0d803bcb264f5ec5854c0c8dd9c3f3
the-world-serpent <samrat.saha.1121@gmail.com>
2021-06-10 04:02:40 +0530

    new changes

--- js/signup.js
+++
@@ -1,6 +0,0 @@
-function checkPass(){
-    if(document.getElementById('password').value!=document.getElementById('r_password').value){
-        alert("password didn't match");
-        return false;
-    }
-}
